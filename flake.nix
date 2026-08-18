01d6b1e3be618945eb3387b537df96be7ac0693f
WinterSunst95 <wintersunset95@gmail.com>
2026-08-18 14:01:10 +0530

    feat: nixified the dev environment

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "peek-a-boo.ts - Scraping Library Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+            corepack_22
+            typescript-language-server
+          ];
+
+          shellHook = ''
+            echo "🕵️ peek-a-boo.ts Dev Environment Loaded"
+            corepack enable pnpm
+          '';
+        };
+      }
+    );
+}
